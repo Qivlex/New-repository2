de458b299d1a22c07a091ac7299fb2ac526a785b
Qivlex <72989086+Qivlex@users.noreply.github.com>
2024-05-24 14:22:23 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="page-footer teal lighten-1">
             <div className="container">
-                © {new Date().getFullYear()} All rights reserved
+                © {new Date().getFullYear()} <a href="https://github.com/Qivlex">Кириллов Максим</a>
             </div>
         </footer>
     );
